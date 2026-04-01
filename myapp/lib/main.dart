c938cf9d23c8b3c7b30e15aba01a30bf00d557a7
Vorth <boscrypto740@gmail.com>
2026-04-01 02:41:31 +0000

    feat: setup Firebase + Flutter base project

---
+++ myapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Firebase Connected!')),
+        body: const Center(child: Text('Hello Firebase! 🔥')),
+      ),
+    );
+  }
+}
